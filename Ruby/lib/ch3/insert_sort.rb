f07b4f30317f6c2adc5b10ce0eae73a4ea1b7f15
Calin <calinoiu.alexandru@agilefreaks.com>
2012-06-11 12:38:17 +0300

    binary tree solution (no memory)

--- Ruby/lib/ch3/insert_sort.rb
+++ Ruby/lib/ch3/insert_sort.rb
@@ -1,15 +1,57 @@
+class Node
+  attr_accessor :value, :occurrences, :children_count
+
+  def initialize(value, occurrences = 1)
+    @value = value
+    @occurrences = occurrences
+    @children_count = 0
+  end
+end
+
 class InsertSort
   def number_of_swaps(input)
     # skip over the number of elements
     input.gets()
-    array = input.gets().split.map { |n| n.to_i }
+    array = input.gets().split.map { |n| Node.new(n.to_i, 1) }
+    tree = []
     result = 0
 
-    array.each_with_index do |e, i|
-      (i - 1).downto(0) do |j|
-        if array[j] > e
-          result +=1
+    array.each do |e|
+      i = 1
+      until tree[i].nil? do
+        if tree[i].value < e.value
+          # go right
+          i = 2*i + 1
+        elsif tree[i].value > e.value
+          result += tree[i].occurrences
+
+          # add right child occurrences
+          unless tree[2*i + 1].nil?
+            result += tree[2*i + 1].children_count + 1
+          end
+
+          # go left
+          i = 2*i
+        else
+          # add right child occurrences
+          unless tree[2*i + 1].nil?
+            result += tree[2*i + 1].children_count + 1
+          end
+
+          break
         end
+      end
+
+      if tree[i].nil?
+        tree[i] = e
+      elsif tree[i].value == e.value
+        tree[i].occurrences += 1
+      end
+
+      # increment children count
+      until tree[i / 2].nil?
+        tree[i / 2].children_count += 1
+        i /= 2
       end
     end
 
@@ -17,12 +59,11 @@ class InsertSort
   end
 end
 
-#input = STDIN
-input = File.new("input01.txt", "r")
+input = STDIN
+#input = File.new("input02.txt", "r")
 number_of_test_cases = input.gets.chomp().to_i
 insert_sort = InsertSort.new
 
 number_of_test_cases.times do
-  # ignore the number of inputs
   STDOUT.puts insert_sort.number_of_swaps(input)
 end
