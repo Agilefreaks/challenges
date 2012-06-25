b026459a70c91f3b98ea61f1b82b6e9b45201c41
Calin <calinoiu.alexandru@agilefreaks.com>
2012-06-25 11:17:30 +0300

    first version of the weapon of choice

--- Ruby/lib/ch5/weapon.rb
+++ Ruby/lib/ch5/weapon.rb
@@ -1,3 +1,66 @@
-class Weapon
+require "matrix"
 
+class Matrix
+  def []=(i, j, x)
+    @rows[i][j] = x
+  end
 end
+
+class Weapon
+  attr_accessor :swap_matrix
+  attr_accessor :configuration
+
+  def initialize(size = 3)
+    @swap_matrix = Matrix.zero(size)
+    @configuration = []
+  end
+
+  def can_swap?(index1, index2)
+    result = @swap_matrix[index1, index2] == 0 ? false : true
+
+    unless result
+      # check for indirect swap
+      row1 = @swap_matrix.row(index1)
+      row2 = @swap_matrix.row(index2)
+
+      row1.to_a.each_index do |index|
+        if row1[index] == 1 && row2[index] == 1
+          result = true
+          @swap_matrix[index1, index2] = 1
+          @swap_matrix[index2, index1] = 1
+          break
+        end
+      end
+    end
+
+    result
+  end
+
+  def calibrate
+    0.upto(@configuration.length - 1) do |i|
+      (i + 1).upto(@configuration.length - 1) do |j|
+        if @configuration[i] > @configuration[j] && can_swap?(i, j)
+          @configuration[i], @configuration[j] = @configuration[j], @configuration[i]
+        end
+      end
+    end
+
+    @configuration
+  end
+end
+
+#input = File.new("input00.txt", "r")
+input = STDIN
+output = STDOUT
+
+k = input.gets().chomp().to_i
+weapon = Weapon.new(k)
+weapon.configuration = input.gets().split().map { |value| value.to_i }
+0.upto(k - 1) do |index|
+  line = input.gets().chomp()
+  0.upto(line.length - 1).each do |i|
+    weapon.swap_matrix[index, i] = line[i] == 'N' ? 0 : 1
+  end
+end
+
+output.puts weapon.calibrate.join(" ")
--- Ruby/spec/ch5/weapon_spec.rb
+++ Ruby/spec/ch5/weapon_spec.rb
@@ -3,4 +3,53 @@ require "weapon"
 
 describe Weapon do
   it { should be_a(Weapon) }
+
+  describe :can_swap do
+    it "should return true for a direct swap" do
+      subject.swap_matrix = Matrix[[0, 0, 0],
+                                   [0, 1, 1],
+                                   [0, 1, 0]]
+      subject.can_swap?(1, 1).should == true
+    end
+    it "should return false when no direct swap" do
+      subject.swap_matrix = Matrix[[0, 0, 0],
+                                   [0, 1, 1],
+                                   [0, 1, 0]]
+      subject.can_swap?(0, 0).should == false
+    end
+    it "should return true for indirect swaps" do
+      subject.swap_matrix = Matrix[[0, 0, 1],
+                                   [0, 0, 1],
+                                   [1, 1, 0]]
+      subject.can_swap?(0, 1).should == true
+      subject.swap_matrix[0, 1].should == 1
+      subject.swap_matrix[1, 0].should == 1
+    end
+    it "should return true for inverse indirect swaps" do
+      subject.swap_matrix = Matrix[[0, 0, 1],
+                                   [0, 0, 1],
+                                   [1, 1, 0]]
+      subject.can_swap?(1, 0).should == true
+      subject.swap_matrix[0, 1].should == 1
+      subject.swap_matrix[1, 0].should == 1
+    end
+  end
+
+  describe :calibrate do
+    it "should se direct swaps" do
+      subject.swap_matrix = Matrix[[0, 0, 1],
+                                   [0, 0, 0],
+                                   [1, 0, 0]]
+      subject.configuration = [3, 1, 2]
+      subject.calibrate.should == [2, 1, 3]
+    end
+    it "should see indirect swaps" do
+      subject.swap_matrix = Matrix[[0, 0, 1, 0],
+                                   [0, 0, 1, 1],
+                                   [1, 1, 0, 0],
+                                   [0, 0, 1, 0]]
+      subject.configuration = [4, 3, 2, 1]
+      subject.calibrate.should == [1, 2, 3, 4]
+    end
+  end
 end
