b134861b25058475a44900db275fcef2ff5f6fad
cristibadila <Cristi.Badila@gmail.com>
2012-06-25 12:47:32 +0300

    done

---
+++ CSharp/WeaponOfMassDestruction/WeaponOfMassDestruction/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WeaponOfMassDestruction
+{
+    public class Solution
+    {
+        static void Main(string[] args)
+        {
+            var textReader = GetTextReader();
+            var k = int.Parse(textReader.ReadLine());
+            var values = textReader.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var bitArrays = new List<BitArray>();
+            for (var i = 0; i < k; i++)
+            {
+                var blockConnections = textReader.ReadLine();
+                var bitArray = new BitArray(k);
+                for (var j = 0; j < k; j++)
+                {
+                    bitArray[j] = blockConnections[j] == 'N' ? false : true;
+                }
+
+                AddNewLine(bitArrays, bitArray);
+            }
+
+            Console.WriteLine(SortList(bitArrays, values).Aggregate(string.Empty, (result, value) => result + value + " ").Trim());
+            //Console.ReadKey();
+        }
+
+        private static TextReader GetTextReader()
+        {
+            return Console.In;
+            //return new StreamReader("input00.txt");
+        }
+
+        public static void AddNewLine(List<BitArray> bitArrays, BitArray bitArray)
+        {
+            if (!bitArrays.Any())
+            {
+                bitArrays.Add(bitArray);
+            }
+            else
+            {
+                var valueIndex = bitArrays.Count;
+                var connectedPoints = bitArrays.Where(p => p[valueIndex]).ToList();
+                var orResult = connectedPoints.Aggregate(new BitArray(bitArray), (current, connectedPoint) => current.Or(connectedPoint));
+                foreach (var connectedPoint in connectedPoints)
+                {
+                    var indexOfPoint = bitArrays.IndexOf(connectedPoint);
+                    bitArrays.Remove(connectedPoint);
+                    bitArrays.Insert(indexOfPoint, orResult);
+                }
+
+                bitArrays.Add(orResult);
+            }
+        }
+
+        public static IList<int> SortList(List<BitArray> bitArrays, List<int> values)
+        {
+            var sortedList = new int?[bitArrays.Count];
+            for (var i = bitArrays.Count - 1; i >= 0; i--)
+            {
+                var actualNumber = i + 1;
+                var indexAtEntry = values.IndexOf(actualNumber);
+                var elementPaths = bitArrays[indexAtEntry];
+                if (IsFalseArray(elementPaths))
+                {
+                    sortedList[indexAtEntry] = actualNumber;
+                }
+                else
+                {
+                    for (var j = elementPaths.Count - 1; j >= 0; j--)
+                    {
+                        if (elementPaths[j] && !sortedList[j].HasValue)
+                        {
+                            sortedList[j] = actualNumber;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return sortedList.Select(ni => ni.Value).ToList();
+        }
+
+        private static bool IsFalseArray(BitArray bitArray)
+        {
+            var orResult = false;
+            for (var i = 0; i < bitArray.Length; i++)
+            {
+                orResult |= bitArray[i];
+            }
+
+            return orResult == false;
+        }
+    }
+}
---
+++ CSharp/WeaponOfMassDestruction/WeaponOfMassDestructionTests/SolutionTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using SharpTestsEx;
+using WeaponOfMassDestruction;
+
+namespace WeaponOfMassDestructionTests
+{
+    [TestFixture]
+    public class SolutionTests
+    {
+        [Test]
+        public void AddNewLine_NoOtherLines_AddNewLine()
+        {
+            var pathMatrix = new List<BitArray>();
+            var path = new BitArray(5);
+
+            Solution.AddNewLine(pathMatrix, path);
+
+            pathMatrix.Count.Should().Be.EqualTo(1);
+            pathMatrix[0].Should().Be.EqualTo(path);
+        }
+
+        [Test]
+        public void AddNewLine_PreviousLinesExistButArNotConnectedToCurrentLine_AddsLine()
+        {
+            var pathMatrix = new List<BitArray> { new BitArray(5) };
+            var path = new BitArray(5);
+
+            Solution.AddNewLine(pathMatrix, path);
+
+            pathMatrix.Count.Should().Be.EqualTo(2);
+            Assert.AreEqual(path, pathMatrix[1]);
+        }
+
+        [Test]
+        public void AddNewLine_PreviousLineExistAndArConnectedToCurrentLine_AddsLineAndSetsAllConnectedLinesIncludingSelfToResultOfOrOperation()
+        {
+            var pathMatrix = new List<BitArray>
+                                 {
+                                     new BitArray(new[] {false, false, false, true}),
+                                     new BitArray(new[] {false, false, true, false}),
+                                 };
+            var path = new BitArray(new[] { false, true, false, true });
+
+            Solution.AddNewLine(pathMatrix, path);
+
+            pathMatrix.Count.Should().Be.EqualTo(3);
+            Assert.AreEqual(new BitArray(new[] { false, false, false, true }), pathMatrix[0]);
+            Assert.AreEqual(new BitArray(new[] { false, true, true, true }), pathMatrix[1]);
+            Assert.AreEqual(new BitArray(new[] { false, true, true, true }), pathMatrix[2]);
+        }
+
+        [Test]
+        public void SortList_Always_SetsThePointWithHighestValueToPositionFurthestPosition()
+        {
+            var bitArrays = new List<BitArray>
+                                {
+                                    new BitArray(new [] { true, true, true, true }),
+                                    new BitArray(new [] { true, true, true, true }),
+                                    new BitArray(new [] { true, true, true, true }),
+                                    new BitArray(new [] { true, true, true, true }),
+                                };
+
+            var sortedList = Solution.SortList(bitArrays, new List<int> { 4, 3, 2, 1 });
+
+            Assert.AreEqual(new List<int> { 1, 2, 3, 4 }, sortedList);
+        }
+    }
+}
