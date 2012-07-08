780f4729772b57fa44e18c9fc844f12a4c004596
Calin <calinoiu.alexandru@agilefreaks.com>
2012-07-08 15:11:07 +0300

    update spec_helper

---
+++ Ruby/spec/ch6/repair_roads_spec.rb
@@ -0,0 +1,6 @@
+require "spec_helper"
+require "repair_roads"
+
+describe RepairRoads do
+  it { should be_a(RepairRoads) }
+end
--- Ruby/spec/spec_helper.rb
+++ Ruby/spec/spec_helper.rb
@@ -1,9 +1,7 @@
 APP_ROOT = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-$: << File.join(APP_ROOT, "lib/ch1")
-$: << File.join(APP_ROOT, "lib/ch2")
-$: << File.join(APP_ROOT, "lib/ch3")
-$: << File.join(APP_ROOT, "lib/ch4")
-$: << File.join(APP_ROOT, "lib/ch5")
+(1..6).each do |i|
+  $: << File.join(APP_ROOT, "lib/ch#{i}")
+end
 
 RSpec.configure do |config|
   # ## Mock Framework
