f1b80b23e551d246a915efded8c46e21d4b1c89b
Calin <calinoiu.alexandru@agilefreaks.com>
2012-07-16 11:16:58 +0300

    solved

--- Ruby/lib/ch8/nuclear_bomb_man.rb
+++ Ruby/lib/ch8/nuclear_bomb_man.rb
@@ -1,3 +1,70 @@
 class NuclearBombMan
+  attr_accessor :lines, :matched_lines, :matched_columns, :used
 
+  def initialize(lines = [])
+    @lines = lines
+    @matched_lines = []
+    @matched_columns = []
+    @used = []
+  end
+
+  def bfs(u)
+    return false if @used[u]
+
+    @used[u] = true
+
+    # direct match
+    @lines[u].each do |v|
+      if @matched_columns[v].nil?
+        @matched_lines[u] = v
+        @matched_columns[v] = u
+        return true
+      end
+    end
+
+    @lines[u].each do |v|
+      if bfs(@matched_columns[v])
+        @matched_lines[u] = v
+        @matched_columns[v] = u
+        return true
+      end
+    end
+
+    false
+  end
+
+  def solve
+    was_matched = true
+
+    while was_matched
+
+      was_matched = false
+      0.upto(@lines.count - 1) { |index| @used[index] = false }
+
+      0.upto(@lines.count - 1) do |index|
+        if @matched_lines[index].nil?
+          was_matched ||= bfs(index)
+        end
+      end
+    end
+
+    @matched_lines.compact.count
+  end
 end
+
+input = STDIN
+output = STDOUT
+
+number_of_lines, number_of_columns = input.gets().chomp().split.map { |i| i.to_i }
+@lines = []
+0.upto(number_of_lines - 1) do |line_index|
+  line = input.gets().chomp().split()
+  @lines[line_index] ||= []
+  line.each_with_index do |e, col_index|
+    if e == '1'
+      @lines[line_index] << col_index
+    end
+  end
+end
+
+output.puts NuclearBombMan.new(@lines).solve
--- Ruby/spec/ch8/nuclear_bomb_man_spec.rb
+++ Ruby/spec/ch8/nuclear_bomb_man_spec.rb
@@ -3,4 +3,36 @@ require "nuclear_bomb_man"
 
 describe NuclearBombMan do
   it { should be_a(NuclearBombMan) }
+
+  describe "bfs" do
+    it "should return true for a direct match" do
+      subject.lines = [[0, 2, 4]]
+      subject.bfs(0).should == true
+      subject.matched_lines[0].should == 0
+      subject.matched_columns[0].should == 0
+    end
+    it "should choose the next available column if the case might be" do
+      subject.lines = [[0, 2, 4]]
+      subject.matched_columns[0] = 0
+
+      subject.bfs(0).should == true
+      subject.matched_lines[0].should == 2
+      subject.matched_columns[2].should == 0
+    end
+    it "should recurse to find an alternative" do
+      subject.lines = [[0, 2, 4], [1, 3], [], [0]]
+      subject.matched_columns[0] = 0
+      subject.matched_columns[1] = 1
+      subject.bfs(3).should == true
+      subject.matched_lines[0].should == 2
+      subject.matched_lines[3].should == 0
+    end
+  end
+
+  describe "solve" do
+    it "should solved the problem" do
+      subject.lines = [[0, 2, 4], [1, 3], [], [0]]
+      subject.solve.should == 3
+    end
+  end
 end
